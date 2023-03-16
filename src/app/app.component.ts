4cdb686545035d8f7c0e83da465b109dd89c26f2
ROMAN BADILLO GONZALEZ <rbadillo@elektra.com.mx>
2023-03-16 15:43:42 -0600

    Complete behavior

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,55 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ng-stopwatch';
+  ms: any = '0' + 0;
+  sec: any = '0' + 0;
+  min: any = '0' + 0;
+  hr: any = '0' + 0;
+
+  startTimer: any;
+  running = false;
+
+  start() {
+    if (!this.running) {
+      this.running = true;
+      this.startTimer = setInterval(() => {
+        this.ms++;
+        this.ms = this.ms < 10 ? '0' + this.ms : this.ms;
+
+        if (this.ms === 100) {
+          this.sec++;
+          this.sec = this.sec < 10 ? '0' + this.sec : this.sec;
+          this.ms = '0' + 0;
+        }
+
+        if (this.sec === 60) {
+          this.min++;
+          this.min = this.min < 10 ? '0' + this.min : this.min;
+          this.sec = '0' + 0;
+        }
+
+        if (this.min === 60) {
+          this.hr++;
+          this.hr = this.hr < 10 ? '0' + this.hr : this.hr;
+          this.min = '0' + 0;
+        }
+      }, 10);
+    } else {
+      this.stop();
+    }
+  }
+
+  stop() {
+    clearInterval(this.startTimer);
+    this.running = false;
+  }
+
+  reset() {
+    clearInterval(this.startTimer);
+    this.running = false;
+    this.hr = this.min = this.sec = this.ms = '0' + 0;
+  }
 }
